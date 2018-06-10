fc953698bb492cabe69321674b496a3937f05102
Ajay Sharma <ajsharm@microsoft.com>
2018-06-10 18:40:08 +0530

    Added Int To Roman

--- IntToRoman.py
+++ IntToRoman.py
@@ -1,16 +1,20 @@
 class Solution:
     def intToRoman(self, num):
-        refdict = dict([(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M')])
-        symbols = [1, 5, 10, 50, 100, 500, 1000]
+        symbols = [(1, 'I'), (4, 'IV'), (5, 'V'), (9, 'IX'), (10, 'X'), (40, 'XL'), (50, 'L'), (90, 'XC'), (100, 'C'), (400, 'CD'), (500, 'D'), (900, 'CM'), (1000, 'M')]
         p = len(symbols)-1
         result = []
-        while num%symbols[p]>0:
-            if int(num/symbols[p])>0:
-                q = int(num/symbols[p])
-                num = num%symbols[p]
-                result += [refdict[symbols[p]] for i in range(q)]
-                p += -1
-            elif p>0 and num>=(4*(symbols[p]-symbols[p-1])/5)
-
+        while (p>0) and (num%symbols[p][0]>0):
+            #print("num:", num, "div:", symbols[p], "p:", p)
+            if int(num/symbols[p][0])>0:
+                q = int(num/symbols[p][0])
+                num = num%symbols[p][0]
+                result += [symbols[p][1] for i in range(q)]
+            p += -1
+            #print("num:", num, "div:", symbols[p], "p:", p, "r:", num%symbols[p])
+        if int(num/symbols[p][0])>0:
+            q = int(num/symbols[p][0])
+            result += [symbols[p][1] for i in range(q)]
+        return ''.join(result)
 sol = Solution()
-print(sol.intToRoman(2))
+inp = int(input())
+print(sol.intToRoman(inp))
